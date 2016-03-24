665cfa4ecf73aa79328f1b3a61d55aa851871c04
1337hax0r <1337hax0r@jaccck.com>
2016-03-24 23:00:02 +0000

    1337
    Well done. now what?

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Hack Your Friends (:</title>
+        <style>
+            body {
+                color: #27ff00;
+                background: #000;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Hack Your Friends (:</h1>
+        <h2>Pricings</h2>
+        <ul>
+            <li>Facebook Password: <b>&pound;25</b></li>
+            <li>Any other specific Password: <b>&pound;20</b></li>
+            <li>Any specific file from Computer: <b>&pound;10</b></li>
+            <li>Browser History: <b>&pound;10</b></li>
+            <li>School Login Details: <b>&pound;10</b></li>
+            <li>Computer IP Address: <b>&pound;5</b></li>
+        </ul>
+        <h2>NO RL CONTACT</h2>
+        <h2>Have a nice day (:</h2>
+    </body>
+</html>
